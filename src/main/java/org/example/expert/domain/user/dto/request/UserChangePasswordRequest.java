577175fb75190753ff3972d3cc1608bcaf2a6f0b
joansdev <joans.dev@gmail.com>
2025-06-12 12:34:53 +0900

    refactor: 코드 개선: validation

--- src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
+++ src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
@@ -1,17 +1,31 @@
 package org.example.expert.domain.user.dto.request;
 
-import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Setter;
 
 @Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserChangePasswordRequest {
 
-    @NotBlank
-    private String oldPassword;
-    @NotBlank
+    @NotBlank(message = "새 비밀번호는 필수입니다.")
+    @Size(min = 8, message = "새 비밀번호는 최소 8자 이상이어야 합니다.")
+    @Pattern(
+            regexp = "^(?=.*[A-Z])(?=.*\\d).+$",
+            message = "새 비밀번호는 숫자와 대문자를 포함해야 합니다."
+    )
     private String newPassword;
+
+    @NotBlank(message = "현재 비밀번호는 필수입니다.")
+    private String oldPassword;
+
+    public String getOldPassword() {
+        return oldPassword;
+    }
 }
